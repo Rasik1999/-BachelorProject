f4a9ce5aa66eccc48b443370bc8da448b0c72b7f
rasul.ramazanov2017@gmail.com <rasul.ramazanov2017@gmail.com>
2021-05-03 17:52:35 +0300

    Added ProgressController

---
+++ WorkingWithProjects.API/Controllers/ProgressController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WorkingWithProjects.API.Models;
+using WorkingWithProjects.DATA;
+
+namespace WorkingWithProjects.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProgressController : ControllerBase
+    {
+        private readonly IProgressRepository _progressRepository;
+
+        public ProgressController(IProgressRepository progressRepository)
+        {
+            _progressRepository = progressRepository;
+        }
+
+        // GET: api/<ProgressController>
+        [HttpGet]
+        public IEnumerable<Progress> Get()
+        {
+            return _progressRepository.GetAllProgress();
+        }
+
+        // GET api/<ProgressController>/5
+        [HttpGet("{id}")]
+        public Progress Get(int id)
+        {
+            return _progressRepository.GetProgressById(id);
+        }
+    }
+}
