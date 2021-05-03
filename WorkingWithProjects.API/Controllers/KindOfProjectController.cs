c5734494ca4df45d1e348fc33ca2e1f1412e1167
rasul.ramazanov2017@gmail.com <rasul.ramazanov2017@gmail.com>
2021-05-03 15:09:02 +0300

    Deleted unused usings

--- WorkingWithProjects.API/Controllers/KindOfProjectController.cs
+++ WorkingWithProjects.API/Controllers/KindOfProjectController.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using WorkingWithProjects.API.Models;
 using WorkingWithProjects.DATA;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WorkingWithProjects.API.Controllers
 {
